9c8881e98ab27fc0408d6b08ff9e281a3d6f4267
Brijesh <softbrijeshio@gmail.com>
2025-10-09 12:37:53 +0530

    feat(web): add home page layout and content

---
+++ web/src/data/index.ts
@@ -0,0 +1,49 @@
+import { CodeXml, Pin, Save, Search } from "lucide-react";
+
+export const features = [
+  {
+    Icon: Save,
+    title: "Save Snippets Instantly",
+    description:
+      "Quickly save any piece of code form anywhere with just a few clicks.",
+  },
+  {
+    Icon: Pin,
+    title: "Organize with Tags",
+    description:
+      "Use tags to categorize your snippets for easy retrieval and management.",
+  },
+  {
+    Icon: CodeXml,
+    title: "Syntax Highlighting",
+    description:
+      "Automatically highlight your code snippets with syntax highlighting.",
+  },
+  {
+    Icon: Search,
+    title: "Powerful Smart Search",
+    description:
+      "Find the exact snippet you need in seconds with our intelligent search engine.",
+  },
+];
+
+export const steps = [
+  {
+    step: 1,
+    title: "Create Snippet",
+    description:
+      "Easily create and save new code snippets from your browser or editor.",
+  },
+    {
+    step: 2,
+    title: "Organize & Tag",
+    description:
+      "Add tags and description to keep your code library neat and searchable.",
+  },
+    {
+    step: 3,
+    title: "Access Anywhere",
+    description:
+      "Your snippets are synced to the cloud, accessible from any device.",
+  },
+];
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,3 +1,95 @@
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { features, steps } from "@/data";
+
 export function Home() {
-  return <div>Home Page</div>;
+  return (
+    <>
+      {/* Header Section */}
+      <header className="w-full min-h-[400px] h-[70vh] flex items-center justify-center">
+        <div className="flex flex-col gap-5 text-center">
+          <h1 className="text-4xl font-semibold">
+            Save, Organize & Access <br /> Your Code Snippets <br />
+            Effortlessly
+          </h1>
+          <p className="text-foreground/80">
+            A modern, clean and developer-focused application to boost your
+            productivity.
+          </p>
+          <div className="flex items-center justify-center gap-3">
+            <Link to={"/dashboard"}>
+              <Button>Get Started</Button>
+            </Link>
+
+            <Link to={"/login"}>
+              <Button variant={"outline"}>Login</Button>
+            </Link>
+          </div>
+        </div>
+      </header>
+
+      {/* Why SnippetX Section */}
+      <section className="w-full py-20">
+        <div className="text-center">
+          <h2 className="text-2xl font-semibold">Why SnippetsX</h2>
+          <p className="text-foreground/80">
+            Core features designed to streamline your development workflow.
+          </p>
+        </div>
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-5">
+          {features?.map((feature, index: number) => (
+            <Card key={index}>
+              <CardHeader>
+                <feature.Icon size={25} className="text-primary mb-1" />
+                <CardTitle>{feature.title}</CardTitle>
+                <p className="text-foreground/80">{feature.description}</p>
+              </CardHeader>
+            </Card>
+          ))}
+        </div>
+      </section>
+
+      {/* Steps Section */}
+      <section className="w-full py-20">
+        <div className="text-center">
+          <h2 className="text-2xl font-semibold">
+            Get Started in 3 Simple Steps
+          </h2>
+          <p className="text-foreground/80">
+            Start organizing your code in minutes.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-5">
+          {steps?.map((step, index: number) => (
+            <div key={index} className="flex flex-col gap-1 text-center">
+              <div className="flex items-center justify-center bg-primary p-5 rounded-full w-8 h-8 mx-auto">
+                <span className="text-xl text-accent">{step.step}</span>
+              </div>
+              <h3 className="text-xl font-semibold">{step.title}</h3>
+              <p className="text-foreground/80">{step.description}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* Get Started Section  */}
+      <section className="w-full py-20 bg-blue-600 rounded-2xl">
+        <div className="text-center px-4">
+          <h2 className="text-2xl font-semibold">
+            Ready to Boost your Productivity
+          </h2>
+          <p className="text-foreground/80">
+            Stop searching for the same code over and over. Start building your
+            personal snippet library today.
+          </p>
+          <div className="mt-5">
+            <Link to="/register">
+              <Button>Sign up now for Free</Button>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
