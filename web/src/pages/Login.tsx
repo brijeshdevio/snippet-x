ed248eeab0fcaf6a7c6a928e1bb33434bbd41dee
Brijesh <softbrijeshio@gmail.com>
2025-10-10 10:56:59 +0530

    feat(web): add login page layout and content

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,3 +1,40 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import { Link } from "react-router-dom";
+
 export function Login() {
-  return <div>Login Page</div>;
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-center text-2xl">Welcome back</CardTitle>
+          <form className="flex flex-col gap-3 mt-3">
+            <div className="flex flex-col gap-1">
+              <Label>Email Address</Label>
+              <Input placeholder="jasmine.doe@example.com" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <Label>Password</Label>
+              <Input placeholder="*********" />
+            </div>
+            <div className="text-center">
+              <Button className="w-2/3">Log in</Button>
+            </div>
+          </form>
+          <Separator className="my-3" />
+          <div className="text-center">
+            <p>
+              Don't have an account?{" "}
+              <Link className="underline text-primary" to={"/register"}>
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 }
