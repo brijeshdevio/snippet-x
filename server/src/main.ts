529c2bd48fa2bb462fad605add4b8f3a72e873cb
Brijesh <softbrijeshio@gmail.com>
2025-10-13 12:55:02 +0530

    feat(api): enable CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,15 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const hosts = process.env.HOSTS_URI as string;
+const allowHosts = hosts?.split(' ');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+  app.enableCors({
+    origin: allowHosts,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap()
