9834979d4ce72c817e0a151a020ebaaa5ec81f65
Brijesh <softbrijeshio@gmail.com>
2025-10-17 17:17:56 +0530

    feat(web): add responsive navbar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Blocks } from "lucide-react";
+import { Blocks, Menu } from "lucide-react";
 import { Button } from "./ui/button";
 import { useAuth } from "@/auth";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+
+const authNavLinks = [
+  { name: "Dashboard", href: "/dashboard" },
+  { name: "Snippets", href: "/snippets" },
+  { name: "New Snippet", href: "/snippets/new" },
+];
+
+const resNavLinks = [
+  { name: "Login", href: "/login" },
+  { name: "Sign Up", href: "/register" },
+];
 
 export function Navbar() {
   const { isAuthenticated } = useAuth();
+  const [open, setOpen] = useState(false);
 
   return (
-    <nav className="w-full py-2 px-3 sm:px-5 bg-secondary">
+    <nav className="w-full py-2 px-3 sm:px-5 bg-secondary border-b">
       <div className="w-full sm:w-[90%] flex items-center justify-between mx-auto">
         {/* Logo */}
         <Link to="/" className="flex items-center gap-1">
@@ -18,7 +32,7 @@ export function Navbar() {
         </Link>
 
         {/* Right Links */}
-        <div className="flex items-center gap-2">
+        <div className="hidden md:flex items-center gap-2">
           <Link to={"/"}>
             <Button variant={"link"} size={"sm"} className="text-foreground">
               Home
@@ -26,34 +40,17 @@ export function Navbar() {
           </Link>
           {isAuthenticated ? (
             <>
-              <Link to={"/dashboard"}>
-                <Button
-                  variant={"link"}
-                  size={"sm"}
-                  className="text-foreground"
-                >
-                  Dashboard
-                </Button>
-              </Link>
-              <Link to={"/snippets"}>
-                <Button
-                  variant={"link"}
-                  size={"sm"}
-                  className="text-foreground"
-                >
-                  Snippets
-                </Button>
-              </Link>
-              <Link to={"/snippets/new"}>
-                <Button
-                  variant={"link"}
-                  size={"sm"}
-                  className="text-foreground"
-                >
-                  Create Snippet
-                </Button>
-              </Link>
-
+              {authNavLinks.map((link) => (
+                <Link to={link.href} key={link.href}>
+                  <Button
+                    variant={"link"}
+                    size={"sm"}
+                    className="text-foreground"
+                  >
+                    {link.name}
+                  </Button>
+                </Link>
+              ))}
               <Button variant={"link"} size={"sm"} className="text-foreground">
                 Log Out
               </Button>
@@ -76,6 +73,74 @@ export function Navbar() {
               </Link>
             </>
           )}
+        </div>
+
+        {/* Mobile Menu Button */}
+        <div className="md:hidden">
+          <Sheet open={open} onOpenChange={setOpen}>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon" aria-label="Menu">
+                <Menu size={30} />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="right" className="text-sm ">
+              <div className="flex flex-col items-center space-y-3 mt-10">
+                <Link to={"/"}>
+                  <Button
+                    variant={"link"}
+                    size={"sm"}
+                    className="text-foreground"
+                  >
+                    Home
+                  </Button>
+                </Link>
+                {isAuthenticated ? (
+                  <>
+                    {authNavLinks.map((link) => (
+                      <Link
+                        key={link.name}
+                        to={link.href}
+                        onClick={() => setOpen(false)}
+                        className="font-medium transition"
+                      >
+                        <Button
+                          variant={"link"}
+                          size={"sm"}
+                          className="text-foreground"
+                        >
+                          {link.name}
+                        </Button>
+                      </Link>
+                    ))}
+                    <Button
+                      variant={"ghost"}
+                      size={"sm"}
+                      className="text-foreground"
+                    >
+                      Log Out
+                    </Button>
+                  </>
+                ) : (
+                  resNavLinks.map((link) => (
+                    <Link
+                      key={link.name}
+                      to={link.href}
+                      onClick={() => setOpen(false)}
+                      className="font-medium transition"
+                    >
+                      <Button
+                        variant={"link"}
+                        size={"sm"}
+                        className="text-foreground"
+                      >
+                        {link.name}
+                      </Button>
+                    </Link>
+                  ))
+                )}
+              </div>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
     </nav>
