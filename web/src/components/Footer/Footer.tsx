4a196a2909d571deebce556f8a8a2db70ef2ab41
Brijesh <softbrijeshio@gmail.com>
2025-12-13 13:36:56 +0530

    feat(web): add content into footer component

--- web/src/components/Footer/Footer.tsx
+++ web/src/components/Footer/Footer.tsx
@@ -1,3 +1,9 @@
 export function Footer() {
-  return <nav>Footer</nav>;
+  return (
+    <footer className="w-full py-3 bg-base-100 border-t border-white/10">
+      <div className="container mx-auto text-center text-sm opacity-70">
+        <p>© {new Date().getFullYear()} SnippetX. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
