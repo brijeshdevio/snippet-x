35af89e57b1ca065a71415ee4df7075ebe7d08c1
Brijesh <softbrijeshio@gmail.com>
2025-12-24 15:34:04 +0530

    fix(web): fix user fetching state

--- web/src/routes/ProtectedRoute.tsx
+++ web/src/routes/ProtectedRoute.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "@/auth";
+import { Loader } from "@/components";
 
 export default function ProtectedRoute() {
   const { user, loading } = useAuth();
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <Loader className="h-screen" />;
   if (!user) return <Navigate to="/login" replace />;
 
   return <Outlet />;
--- web/src/routes/RestrictedRoute.tsx
+++ web/src/routes/RestrictedRoute.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "@/auth";
+import { Loader } from "@/components";
 
 export default function RestrictedRoute() {
   const { user, loading } = useAuth();
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <Loader className="h-screen" />;
   if (user) return <Navigate to="/dashboard" replace />;
 
   return <Outlet />;
