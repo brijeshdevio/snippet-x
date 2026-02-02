f565bba7a40c13eb441247526e593d6305dac47f
Brijesh <softbrijeshio@gmail.com>
2026-02-02 12:57:41 +0530

    feat: configure frontend URI for CORS, add Groq API key, and ignore .env files in the web project.

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,18 +2,17 @@ import 'dotenv/config';
 
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
 
-const hosts = process.env.HOSTS_URI as string;
-const allowHosts = hosts?.split(' ');
+const FRONTEND_URI = process.env.FRONTEND_URI!;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
   app.use(cookieParser());
   app.enableCors({
-    origin: allowHosts,
+    origin: FRONTEND_URI,
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
